a457fae6334ea8427b0776345745d5bb961cfd3c
shahrokh kiyani <shahrokhkiyani@pm.me>
2022-08-03 23:24:36 +0430

    add login html and templates for 404 page

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'pages/home.html')
+
+def error404(request, exception):
+    return render(request, '404.html')
+
+def error500(request):
+    return render(request, '404.html')
+    
