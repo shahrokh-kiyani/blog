d1e2cb8067c04a82b3f954e1a782dc26688ad926
shahrokh kiyani <shahrokhkiyani@pm.me>
2022-08-17 16:01:35 +0430

    add search to site

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', ListView.as_view(), name='home'), # NO path used for home to use site easy
     path('detail/<slug:slug>/', DetailView.as_view(), name='detail'), # Detail path have slug because used slug in models
     path('category/', CategoryListView.as_view(), name='category'), # Category path have slug because used slug in models
-    path('category/<slug:slug>/', category_detail_view, name='category_detail'),
+    path('category/<slug:slug>/', category_detail_view, name='category_detail'), # Category deatail is view for see what posts have same category
+    path('search/', search_view, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -39,22 +39,38 @@ class CategoryListView(generic.ListView):
     # context name for template
     context_object_name = 'categories'
 
-
-# Category Detail View show all posts related to category
-# class CategoryDetailView(generic.DetailView):
-#     # Model category from models.py
-#     model = Category
-#     # template name in tepmlates/blog/category_detail.html
-#     template_name = 'blog/category_detail.html'
-#     # context name for template
-#     context_object_name = 'category'
-
+# Category detail view show all posts have same category
 def category_detail_view(request, slug):
+    # Find category and post in database
     category = Category.objects.get(slug=slug)
     posts = Post.objects.filter(category=category)
+    # Context send for template
     context = {
         'category': category,
         'posts': posts
     }
+
     return render(request, 'blog/category_detail.html', context)
-    
+
+
+# This is search view that will search around all database to found a user input
+def search_view(request):
+
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        # Here we check if user input was nothing we redierct him to 404 page
+        if searched == '':
+            return render(request, 'pages/404.html')
+
+        # Here we search database for user search
+        post = Post.objects.filter(title__contains=searched)
+        
+        context = {
+            'searched': searched,
+            'posts': post
+        } 
+
+        return render(request, 'blog/search.html', context)
+
+    else: 
+        return render(request, 'pages/404.html')
