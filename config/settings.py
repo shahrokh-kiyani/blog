a03ff8a154eabe56a1e2c3fa70a2731fa7e2eabd
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-08-22 10:50:44 +0430

    add debug to env file

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
